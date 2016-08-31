3bcd7066faf7de8acf51436121d285fd2c77a83b
4015d17 <4015d17@jobo.ece.uprm.edu>
2016-08-31 15:31:14 -0400

    Move star.

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -82,16 +82,16 @@ public class MyPanelClass extends JPanel {
 		
 
 		Polygon p2 = new Polygon();
-		p2.addPoint(x1 + 25, y1 + 73);
-		p2.addPoint(x1 + 41, y1 + 73);
-		p2.addPoint(x1 + 47, y1 + 58);
-		p2.addPoint(x1 + 53, y1 + 73);
-		p2.addPoint(x1 + 69, y1 + 73);
-		p2.addPoint(x1 + 56, y1 + 83);
-		p2.addPoint(x1 + 61, y1 + 98);
-		p2.addPoint(x1 + 47, y1 + 88);
-		p2.addPoint(x1 + 34, y1 + 98);
-		p2.addPoint(x1 + 38, y1 + 83);
+		p2.addPoint(x1 + 25, y1 + 78);
+		p2.addPoint(x1 + 41, y1 + 78);
+		p2.addPoint(x1 + 47, y1 + 63);
+		p2.addPoint(x1 + 53, y1 + 78);
+		p2.addPoint(x1 + 69, y1 + 78);
+		p2.addPoint(x1 + 56, y1 + 88);
+		p2.addPoint(x1 + 61, y1 + 103);
+		p2.addPoint(x1 + 47, y1 + 93);
+		p2.addPoint(x1 + 34, y1 + 103);
+		p2.addPoint(x1 + 38, y1 + 88);
 		g.setColor(Color.WHITE);
 		g.fillPolygon(p2);
 	}
